af14555b8f5d64b22fd3e633a980a6b881714b13
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2020-01-07 06:32:58 +0100

    update

---
+++ categorize.js
@@ -0,0 +1,62 @@
+const speed = 3;
+const files = {};
+
+var k = 0;
+for (var i in images) {
+  var uri = images[i];
+  if (uri.startsWith('files/')) {
+    files[uri] = [uri];
+  } else {
+    var uri2 = uri.split('/').slice(0, 2).join('/');
+    if (uri2 in files) {
+      files[uri2].push(uri);
+    } else {
+      files[uri2] = [uri];
+    }
+    k++;
+    if (k < 10) {
+      console.log(uri2);
+    }
+  }
+}
+
+const keys = Object.keys(files);
+
+let imgs = [];
+function addImage() {
+  var r = Math.floor(Math.random() * keys.length);
+  var r2 = Math.floor(Math.random() * files[keys[r]].length);
+  var uri = files[keys[r]][r2];
+
+  var img = document.createElement('img');
+  var a = document.createElement('a');
+
+  var fullURI = 'https://opengameart.org/sites/default/' + uri;
+  let href = '';
+  if (uri.startsWith('files/')) {
+    href = links[fullURI];
+    console.log('files', uri, fullURI, href);
+  } else {
+    let f = fullURI.replace('/unpacked/', '/files/');
+    let f2 = f.split('/').slice(0, 7).join('/');
+    href = links[f2];
+    console.log('packed', f2, href);
+  }
+  a.href = 'https://opengameart.org/' + href.replace(/.html$/, '');
+  document.body.append(a);
+  a.append(img);
+  img.src = 'https://opengameart.fra1.cdn.digitaloceanspaces.com/' + encodeURI(uri).replace(/#/g, '%23');
+}
+function scroll() {
+  var doc = document.documentElement;
+  //var left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
+  var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+  var dist = document.body.scrollHeight - top - window.innerHeight;
+  //console.log("dist", dist);
+  if (dist < window.innerHeight) {
+    window.scrollTo(0, document.body.scrollHeight);
+    return true;
+  }
+  return false;
+}
+addImage();
