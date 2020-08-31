92d75a06e0fdafdafcfc086ce6ab7fbe7f46a386
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2020-08-31 22:04:13 +0200

    update

--- src/concat.sh
+++ src/concat.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #cd predict &&
-find /mnt/d/opengameart/unpacked -type f -name \*.np -print0 |
-  sort -z |
-  xargs -0 cat -- >> predict.out
+#find /mnt/d/opengameart/unpacked -type f -name \*.np -print0 |
+#  sort -z |
+#  xargs -0 cat -- >> predict.out
+cat allpaths.txt | xargs -0 cat -- >> predict.out
