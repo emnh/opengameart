1cf58b6d2e0850f4882b3bff84816152dcc52c1f
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2020-08-31 19:06:06 +0200

    add list_files.py

---
+++ src/list_files.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+def getFiles(imageDir, savefile):
+    if not os.path.exists(savefile):
+        paths = []
+        for root, dirs, files in os.walk(imageDir):
+            for file in files:
+                if file.lower().endswith('.np'):
+                    file2 = file[:-len('.np')]
+                    paths.append(os.path.join(root, file2))
+        fd = open(savefile, 'w')
+        fd.writelines([x + '\n' for x in paths])
+        fd.close()
+    else:
+        fd = open(savefile, 'r')
+        paths = [x.rstrip() for x in fd.readlines()]
+        fd.close()
+    return paths
+
+if __name__ == '__main__':
+    spriteDir = '/mnt/i/opengameart/sprites'
+    imageDir = '/mnt/i/opengameart/files'
+    unpackedDir = '/mnt/i/opengameart/unpacked'
+    
+    paths = []
+    spritePaths = getFiles(spriteDir, 'sprites.txt')
+    filePaths = getFiles(spriteDir, 'files.txt')
+    unpackedPaths = getFiles(spriteDir, 'unpacked.txt')
+
+    l = len('_00000.png')
+    usedPaths = [(x[:-l] + '.png').replace(spriteDir, imageDir) for x in paths]
+    d = {}
+    for path in usedPaths:
+        d[path] = True
+
+    count = 0
+    noncount = 0
+    for path in filePaths:
+        if path in d:
+            noncount += 1
+        else:
+            count += 1
+            paths.append(path)
+
+    paths.extend(spritePaths)
+    paths.extend(unpackedPaths)
+    paths.sort()
+
+    bigsavefile = 'allpaths.txt'
+    fd = open(bigsavefile, 'w')
+    fd.writelines([x + '\n' for x in paths])
+    fd.close()
